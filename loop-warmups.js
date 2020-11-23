e9d6c9af3930822b431930647b10d7f24cc00d07
alexite03 <snipescope360@gmail.com>
2020-11-23 15:27:47 +0100

    uppgift 1

---
+++ loop-warmups.js
@@ -0,0 +1,15 @@
+//
+function whileLoop() {
+    let counter = 0
+    while(counter < 10) {
+        counter++
+        console.log(counter)
+    }
+    console.log("done")
+}
+//
+for(let counter = 0; counter < 10; counter++) {
+    console.log(counter)
+}
+console.log("done")
+//
