c31179eb2612b246ed6a85ec53d16e2bfb4adb99
unknown <alexander.bykadorov@skola.botkyrka.se>
2020-11-30 14:48:48 +0100

    Uppgift 2

--- statistics.js
+++ statistics.js
@@ -1,12 +1,32 @@
-
 function max(arr) {
-
+    let big = 0
+    for(i = 0; i < arr.length; i++) {
+        if(arr[i] > big) {
+            big = arr[i]
+        }
+    }
+return big
 }
 
 function min(arr) {
-
+    let small = 100 // working on finding out how to set unspecified huge infinite number
+    for(i = 0; i < arr.length; i++) {
+        if(arr[i] < small) {
+            small = arr[i]
+        }
+    }
+return small
 }
 
 function mean(arr) {
-    // Ledtråd: Börja med att räkna ut summan av alla element
+    let result = 0
+    
+    for (let i = 0; i < arr.length; i++) {
+       result = result + arr[i];
+    }
+return (result / arr.length)
 }
+
+console.log(max([0, 1, 2, 3, 4, 5]))
+console.log(min([0, 1, 2, 3, 4, 5]))
+console.log(mean([0, 1, 2, 3, 4, 5]))
